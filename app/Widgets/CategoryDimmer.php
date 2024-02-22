de8c61a66df599bb3c806d868c2b7702a93a3f20
abdelghaniAit <aaittamghart8@gmail.com>
2024-02-22 16:26:51 +0100

    Add statistics

---
+++ app/Widgets/CategoryDimmer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Category;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class CategoryDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Category::count();
+        $string = "Categories";
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-categories',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => "View all categories",
+                'link' => route('voyager.categories.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return true;
+    }
+}
---
+++ app/Widgets/OrderDimmer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Widgets;
+
+use App\Order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Str;
+use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Widgets\BaseDimmer;
+
+class OrderDimmer extends BaseDimmer
+{
+    /**
+     * The configuration array.
+     *
+     * @var array
+     */
+    protected $config = [];
+
+    /**
+     * Treat this method as a controller action.
+     * Return view() or other content to display.
+     */
+    public function run()
+    {
+        $count = Order::count();
+        $string = "Orders";
+
+        return view('voyager::dimmer', array_merge($this->config, [
+            'icon'   => 'voyager-basket',
+            'title'  => "{$count} {$string}",
+            'text'   => __('voyager::dimmer.post_text', ['count' => $count, 'string' => Str::lower($string)]),
+            'button' => [
+                'text' => "View all orders",
+                'link' => route('voyager.orders.index'),
+            ],
+            'image' => voyager_asset('images/widget-backgrounds/02.jpg'),
+        ]));
+    }
+
+    /**
+     * Determine if the widget should be displayed.
+     *
+     * @return bool
+     */
+    public function shouldBeDisplayed()
+    {
+        return true;
+    }
+}
