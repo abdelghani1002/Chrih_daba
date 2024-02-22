b94c5c4d60bcd2341aa9ece4f99fb2e9eee54627
mohamed <simotergui4@gmail.com>
2024-02-22 09:23:14 +0100

    change style of cards in   search

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -67,34 +67,40 @@ $(document).ready(function () {
         products.forEach(product => {
             var description = limitString(product.description, 80)
             $("#place_result").append(`
-            <div class="bg-white m-2 rounded-lg overflow-hidden shadow-lg ring-4 ring-red-500 ring-opacity-40 max-w-sm ">
-    <div class="relative w-full">
-                <img class="w-full"  src="http://127.0.0.1:8000/storage/${product.image}" alt="Product Image">
-                <div class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">SALE
-                </div>
+             <a href="http://127.0.0.1:8000/product/${product.id}"
+    class="hover:scale-[101%] bg-white dark:bg-slate-700 m-2 rounded-lg overflow-hidden shadow-lg ring-4 ring-red-500 ring-opacity-40 max-w-sm w-full md:w-[30%] flex flex-col justify-between">
+    <div class="relative w-full  dark:text-gray-300">
+        <img class="w-full object-cover max-h-56" src="http://127.0.0.1:8000/storage/${product.image}" alt="Product Image">
+        <div class="p-2">
+            <div class="absolute top-0 right-0 bg-red-500 text-white px-2 py-1 m-2 rounded-md text-sm font-medium">SALE
             </div>
-            <div class="p-4">
-                <h3 class="text-lg font-medium mb-2">${product.name}</h3>
-                <p class="text-gray-600 text-sm mb-4">${description}</p>
-                <span class="text-lg font-medium mb-2 bg-red-200 py-1 px-2 rounded-md ">${ product.category.name }</span>
-                <div class="flex items-center mt-4 justify-between">
-                    <span class="font-bold text-lg">$ ${  product.price }</span>
-                    <a href="{{route("product.show", $product)}}" class="bg-red-100 px-2 py-1 rounded-sm " >show</a>
-                    <form method="post" action="http://127.0.0.1:8000/product/addtocart">
+            <h3 class="text-lg dark:text-gray-200 font-medium mb-2">${product.name}</h3>
+            <p class="text-gray-600 text-sm mb-4 dark:text-gray-300">${description}</p>
+            <span class="font-medium mb-2 bg-gray-200 dark:bg-gray-600 p-1 rounded-md">${ product.category.name }</span>
+        </div>
+    </div>
+    <div class="p-4">
+        <div class="flex items-center mt-4 justify-between">
+            <span class="font-bold text-lg dark:text-gray-200">$ ${  product.price }</span>
 
-                         <input name="product_id" value="${product.id}" type="hidden">
+                     <form method="post" action="http://127.0.0.1:8000/product/addtocart">
+
+                         <input name="product_id" value="${  product.price }" type="hidden">
                          <input name="_token" value="${token}" type="hidden">
                         <button class="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded">
                             add to cart
                         </button>
                     </form>
-                </div>
-            </div>
+
+
         </div>
+    </div>
+    </a>
 
             `);
         });
     }
+
 
     function limitString(str, maxLength, suffix = '...') {
         if (str.length <= maxLength) {
